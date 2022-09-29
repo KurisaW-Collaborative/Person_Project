45762170426845a0ab20f4f5ca3b56d24ef69e66
KurisaW <2053731441@qq.com>
2022-09-29 13:47:43 +0800

    [add] 重新添加segger_rtt软件包在瑞萨RA6M4开发板上的成功例程

---
+++ segger_rtt/Project/RA_segger_rtt_pr/Debug/rt-thread/components/libc/compilers/common/stdlib.d
@@ -0,0 +1,20 @@
+rt-thread/components/libc/compilers/common/stdlib.o: \
+ ../rt-thread/components/libc/compilers/common/stdlib.c \
+ D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rtconfig_preinc.h \
+ D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtthread.h \
+ D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr/rtconfig.h \
+ D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtdebug.h \
+ D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtdef.h \
+ D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtservice.h \
+ D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtm.h \
+ D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\components\finsh/finsh.h \
+ D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtdbg.h
+D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rtconfig_preinc.h:
+D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtthread.h:
+D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr/rtconfig.h:
+D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtdebug.h:
+D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtdef.h:
+D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtservice.h:
+D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtm.h:
+D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\components\finsh/finsh.h:
+D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rt-thread\include/rtdbg.h:
