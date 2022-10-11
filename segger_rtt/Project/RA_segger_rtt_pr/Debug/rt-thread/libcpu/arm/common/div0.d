2b4559af3f5acf48bde6713c8c361b92ceb9615b
Nedki-L <1535208366@qq.com>
2022-10-11 09:15:05 +0800

    [modify] 删除非必要文件

--- segger_rtt/Project/RA_segger_rtt_pr/Debug/rt-thread/libcpu/arm/common/div0.d
+++
@@ -1,3 +0,0 @@
-rt-thread/libcpu/arm/common/div0.o: ../rt-thread/libcpu/arm/common/div0.c \
- D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rtconfig_preinc.h
-D\:\RT-Thread\RT-ThreadStudio\workback_backup2\RA_segger_rtt_pr\rtconfig_preinc.h:
